c39d06cd0f1a96ab0644ffd570a384b59c7b14c1
Sam njoro <samnjorokibandi@gmail.com>
2025-07-18 16:50:57 +0300

    login axios setup

---
+++ frontend/app/auth/login.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import axiosInstance from "../axios/axiosInstance"
+import {useState} from 'react';
+import { useRouter } from 'next/navigation';
+
+interface FormData {
+    email: string,
+    password: string,
+}
+
+export default function Login (){
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [formData, setFormData]  = useState<FormData>({
+        email: '',
+        password: '',
+    })
+    const router = useRouter();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const { name, value} = e.target;
+        setFormData(prev => ({ ...prev, [name]: value}));
+    }
+
+    const handleSubmit = async (e: {preventDefault: () => void}) => {
+        setLoading(true)
+
+        try{
+            const res= await axiosInstance.post("/user/login", formData)
+            setFormData(res.data);
+
+            router.push('/dash')
+        }catch(err){
+            const error = err instanceof Error ?  err: new Error(String(err))
+            console.error("Error loging:", error)
+        }finally{
+            setLoading(false)
+        }
+    }
+    return(
+        <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input
+                          type="text"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                          required
+                        />
+                    </div>
+                    <div>
+                        <input
+                          type="password"
+                          name="password"
+                          value={formData.password}
+                          onChange={handleChange}
+                          required
+                        />
+                    </div>
+                    <button
+                      type="submit"
+                      className=""
+                    >
+                        {loading ? 'loging ...' : 'login'}
+                    </button>
+
+                </form>
+            </div>
+        </div>
+    )
+
+    
+
+}
